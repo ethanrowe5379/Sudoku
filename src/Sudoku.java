2bdf2e7d0776a7eac189e07701b43346fbffa52f
Ethan Rowe <ethanrowe5379@gmail.com>
2024-06-06 15:36:46 +0100

    Modified the sudoku class. Added all three conditions. Added a failsafe to reset the row if its looped too may times. Comments added. Diary Updated.

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,20 +1,29 @@
-import java.sql.Time;
-import java.util.Arrays;
 import java.util.Random;
 
 public class Sudoku {
 
-    int[][] gameBoard = new int[9][9];
-    int[][][] row = new int[9][9][2];
-    int[][][] col = new int[9][9][2];
-    int[][][] box = new int[9][9][2];
+    public int[][] gameBoard = new int[9][9];
+    private final int[][][] row = new int[9][9][2];
+    private final int[][][] col = new int[9][9][2];
+    private final int[][][] box = new int[9][9][2];
 
+    /*
+        Sudoku() - Constructor
+        Initialises and generates the game board.
+    */
     public Sudoku() {
         System.out.println("Initialising Board");
         this.initialiseGameBoard();
         generateGameBoard();
     }
 
+    /*
+        initialiseGameBoard()
+        Private Method due to only being used within this class.
+        Will Set all values within the game board to 0
+        Sets the row, col and box indexes too.
+        This is to make it easier to search for values.
+     */
     private void initialiseGameBoard(){
         for(int y = 0; y < 9; y++){
             for(int x = 0; x < 9; x++){
@@ -43,6 +52,11 @@ public class Sudoku {
         }
     }
 
+    /*
+        printGameBoard()
+        As it says on the tin. Will print the game board to the console.
+        Utilising a nested for each loop for each element in a row and subsequently each column.
+     */
     private void printGameBoard(){
         for(int[] i: this.gameBoard){
             for(int elem: i){
@@ -52,6 +66,13 @@ public class Sudoku {
         }
     }
 
+    /*
+        x_found(int val, int[][] unit)
+        int val - the value to be checked
+        int[][] unit - the row or col to be checked.
+        Derived from the book "Sudoku programming with C" with minor modifications
+        Couldn't get the box check to work so only works for checking row and col values.
+     */
     public boolean x_found(int val, int[][] unit){
         boolean results = false;
             for(int i = 0; i < 9; i++){
@@ -65,35 +86,55 @@ public class Sudoku {
         return results;
     }
 
+    /*
+        generateGameBoard()
+
+     */
     public void generateGameBoard(){
         Random r = new Random();
         r.setSeed(System.currentTimeMillis());
-        int val = 0;
+        int val;
         for(int y = 0; y < 9; y++){
             for(int x = 0; x < 9; x++){
                 val = getRandom(x, y, r);
+                //If returned Value is 0, checking for valid value has timed out
+                if(val == 0){
+                    resetRow(y); //Resets entire row
+                    x = -1; // Resets the loop
+                    continue;
+                }
                 gameBoard[y][x] = val;
             }
             printGameBoard();
         }
     }
 
-    public int getRandom(int x, int y, Random r){
-        int temp = 0;
+    private void resetRow(int row){
+        for(int x = 0; x < 9; x++) {
+            gameBoard[row][x] = 0;
+        }
+    }
+
+    private int getRandom(int x, int y, Random r){
+        int temp;
+        int counter = 0;
         while(true){
+            counter++;
             temp = r.nextInt(9) + 1;
-            if(!checkBox(temp, box[y][x])) {
+            if(!checkBox(temp, box[y][x]) && !x_found(temp, col[x]) && !x_found(temp, row[y])) {
                 break;
+            }
+            if(counter == 9){
+                return 0;
             }
         }
         return temp;
     }
 
-    public boolean checkBox(int val, int[] unit){
+    private boolean checkBox(int val, int[] unit){
         boolean results = false;
         int iR = unit[0];
         int iC = unit[1];
-        System.out.println(iR + ", "+ iC);
         for(int j = 0; j < 3; j++){
             for(int k = 0; k < 3; k++) {
                 if (val == gameBoard[iR + j][iC + k]) {
